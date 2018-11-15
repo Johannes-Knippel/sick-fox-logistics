a57c6c8d488b8d5676774e4fb0133cc33fa33d0a
Johannes-Knippel <32730055+Johannes-Knippel@users.noreply.github.com>
2018-11-15 19:43:07 +0100

    initial upload
    heppa

---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+declare var H: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'Sick-Fox-logistics';
+
+  private platform: any;
+  public query: string;
+
+  @ViewChild("map")
+  public mapElement: ElementRef;
+
+  public constructor() {
+    this.query = "starbucks";
+    this.platform = new H.service.Platform({
+      "app_id": "mYrOKG6Y0liojLdg0hvQ",
+      "app_code": "At7q4TCeCI6BXaEWXk7Ksw"
+    });
+  }
+
+  public ngOnInit() { }
+
+  public ngAfterViewInit() {
+    let defaultLayers = this.platform.createDefaultLayers();
+    let map = new H.Map(
+      this.mapElement.nativeElement,
+      defaultLayers.normal.map,
+      {
+        zoom: 10,
+        center: { lat: 37.7397, lng: -121.4252 }
+      }
+    );
+  }
+
+}
