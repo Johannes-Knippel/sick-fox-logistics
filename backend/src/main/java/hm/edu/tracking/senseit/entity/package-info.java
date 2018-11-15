09c125e535208364ba0eba982cc90e9ae0d26d7e
Matthias Strobl <matthias.strobl@bmw.de>
2018-11-15 21:31:05 +0100

    cleanup

--- backend/src/main/java/hm/edu/tracking/senseit/entity/package-info.java
+++
@@ -1,9 +0,0 @@
-//======================================================================================================================
-// Copyright (c) 2018 BMW Group. All rights reserved.
-//======================================================================================================================
-/**
- * Contains classes that represent business entities.
- *
- * @author Matthias Strobl, Q253514
- */
-package hm.edu.tracking.senseit.entity;
